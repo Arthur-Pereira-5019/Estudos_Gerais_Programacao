722aa4c8b0576b02a945d202e8af63ff3de7d8b9
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-10-12 21:07:08 -0300

    Iniciado estudos da peneira de Eratóstenes

---
+++ Algoritmos Matemáticos/crivo_de_eratostenes.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class crivo_de_eratostenes {
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        int l;
+        System.out.println("Forneça o limite dos primos");
+        l = r.nextInt();
+        ArrayList<Integer> p = new ArrayList<>();
+        ArrayList<Integer> s = new ArrayList<>();
+        for(int i = 0; i < l; i++) {
+            p.add(i);
+        }
+
+        for(int i = 2; i < p.size(); i++) {
+            int c = p.get(i);
+            s.add(c);
+            for(int j = c; j < p.size(); j++) {
+                if(p.get(j) % c == 0) {
+                    p.remove(j);
+                }
+            }
+        }
+        for(int i = 0; i < s.size(); i++) {
+            System.out.println(s.get(i));
+        }
+    }
+}
